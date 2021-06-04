867021ac239023bfb7c5d2e696e7f08b337634ae
Ramesh <Ramesh@DESKTOP-1O7GECF>
2021-06-04 12:17:36 +0530

    Employee details

---
+++ CoreJava/src/Method/Methods.java
@@ -0,0 +1,15 @@
+package Method;
+
+public class Methods {
+	
+	public void employee() {
+		System.out.println("Employee detaills");
+	}
+		
+
+
+	public static void main(String[] args) {
+		
+	
+	}
+}
